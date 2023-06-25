85b9c07e46f258d47058a859f72d4c8476dce39e
Seungho <hsh47607@naver.com>
2023-06-26 00:14:40 +0900

    Feat: add method of findByUserId

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/repository/UserRepository.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.zerobase.hseungho.restaurantreservation.service.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUserId(String userId);
+
 }
