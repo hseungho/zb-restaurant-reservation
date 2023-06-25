bc4afa38b72360c745a1e7e2daba1dc450616da7
Seungho <hsh47607@naver.com>
2023-06-26 00:14:17 +0900

    Feat: entity impl UserDetail

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/domain/User.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/domain/User.java
@@ -1,19 +1,21 @@
 package com.zerobase.hseungho.restaurantreservation.service.domain;
 
 import com.zerobase.hseungho.restaurantreservation.service.type.UserType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "user")
 @Getter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String id;
@@ -27,4 +29,33 @@ public class User {
     private LocalDateTime updatedAt;
     private LocalDateTime deletedAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(type.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
