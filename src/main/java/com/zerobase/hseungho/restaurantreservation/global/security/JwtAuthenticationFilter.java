c36a3baac43fac9dfd537c0266a76d4130279a09
Seungho <hsh47607@naver.com>
2023-06-26 01:18:44 +0900

    Feat: impl jwt authentication filter

--- src/main/java/com/zerobase/hseungho/restaurantreservation/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/global/security/JwtAuthenticationFilter.java
@@ -6,7 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,10 +21,35 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private static final String TOKEN_HEADER = HttpHeaders.AUTHORIZATION;
+    private static final String TOKEN_PREFIX = "Bearer ";
+
+    private final JwtComponent jwtComponent;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        String token = this.resolveTokenFromRequest(request);
+
+        if (StringUtils.hasText(token) && jwtComponent.validateToken(token)) {
+            Authentication authentication = this.jwtComponent.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info("[MY-LOG] [LOGIN SUCCESS] -> {}", this.jwtComponent.getUserId(token));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveTokenFromRequest(HttpServletRequest request) {
+        String token = request.getHeader(TOKEN_HEADER);
+
+        if (!ObjectUtils.isEmpty(token) && token.startsWith(TOKEN_PREFIX)) {
+            return token.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
     }
 }
