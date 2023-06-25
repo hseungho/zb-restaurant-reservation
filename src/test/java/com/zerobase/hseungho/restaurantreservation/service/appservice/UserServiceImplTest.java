f1a3a5e991179ab2a4973fb466ab63ef977c923d
Seungho <hsh47607@naver.com>
2023-06-26 03:36:29 +0900

    Test: impl sign up biz. logic

--- src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplTest.java
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplTest.java
@@ -1,22 +1,34 @@
 package com.zerobase.hseungho.restaurantreservation.service.appservice;
 
+import com.zerobase.hseungho.restaurantreservation.service.domain.User;
+import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.SignUp;
+import com.zerobase.hseungho.restaurantreservation.service.dto.internal.user.UserDto;
 import com.zerobase.hseungho.restaurantreservation.service.repository.UserRepository;
+import com.zerobase.hseungho.restaurantreservation.service.type.UserType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -58,7 +70,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("userService - 닉네임 중복확인 사용불가")
+    @DisplayName("UserService - 닉네임 중복확인 사용불가")
     void test_checkNicknameAvailable_failure() {
         // given
         given(userRepository.existsByNickname(anyString()))
@@ -67,6 +79,37 @@ class UserServiceImplTest {
         boolean result = userService.checkNicknameAvailable("test_nickname");
         // then
         Assertions.assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("UserService - 회원가입 성공")
+    void test_signUp_success() {
+        // given
+        final String USER_ID = "mock_user_id";
+        final String NICKNAME = "mock_nickname";
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        given(userRepository.save(any()))
+                .willReturn(User.builder()
+                        .userId(USER_ID)
+                        .password("encrypt_password")
+                        .nickname(NICKNAME)
+                        .type(UserType.ROLE_CUSTOMER)
+                        .build()
+                );
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+        // when
+        UserDto userDto = userService.signUp(SignUp.Request.builder()
+                .userId("").password("").nickname("")
+                .build());
+        // then
+        verify(userRepository, times(1)).save(captor.capture());
+        Assertions.assertNotNull(userDto);
+        Assertions.assertEquals(USER_ID, userDto.getUserId());
+        Assertions.assertEquals(NICKNAME, userDto.getNickname());
+        Assertions.assertEquals(UserType.ROLE_CUSTOMER, userDto.getType());
     }
 
 
