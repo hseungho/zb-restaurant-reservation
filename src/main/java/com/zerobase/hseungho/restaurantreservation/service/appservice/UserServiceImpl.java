47dcf8789972905463f84c19ee25506ca44cc23b
Seungho <hsh47607@naver.com>
2023-06-26 00:15:59 +0900

    Feat: impl loadUserByUsername method

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImpl.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zerobase.hseungho.restaurantreservation.service.appservice;
 
+import com.zerobase.hseungho.restaurantreservation.service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService, UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        return userRepository.findByUserId(id).orElseThrow(RuntimeException::new);
     }
 
 }
