58b72f0b00ce2aebb3ae09edac0941fd51c5fe93
Seungho <hsh47607@naver.com>
2023-06-26 02:09:29 +0900

    Test: check id available api

---
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserControllerTest.java
@@ -0,0 +1,78 @@
+package com.zerobase.hseungho.restaurantreservation.service.controller.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.hseungho.restaurantreservation.global.config.SecurityConfiguration;
+import com.zerobase.hseungho.restaurantreservation.global.security.jwt.JwtAuthenticationFilter;
+import com.zerobase.hseungho.restaurantreservation.service.appservice.UserServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+        controllers = UserController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
+        }
+)
+@Import(SecurityConfiguration.class)
+class UserControllerTest {
+
+    @MockBean
+    private UserServiceImpl userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${service.basic-api}")
+    private String BASIC_API;
+    
+    @Test
+    @DisplayName("UserController - 아이디 중복확인 성공 (true)")
+    void test_checkIdAvailable_success_true() throws Exception {
+        // given
+        final String url = BASIC_API + "/sign-up/check-id?id=mocktest12341234";
+
+        given(userService.checkIdAvailable(anyString()))
+                .willReturn(true);
+        // when
+        // then
+        mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value(true));
+    }
+
+    @Test
+    @DisplayName("UserController - 아이디 중복확인 성공 (false)")
+    void test_checkIdAvailable_success_false() throws Exception {
+        // given
+        final String url = BASIC_API + "/sign-up/check-id?id=mocktest12341234";
+
+        given(userService.checkIdAvailable(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        mockMvc.perform(get(url)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value(false));
+    }
+
+}
