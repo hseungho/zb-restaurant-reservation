ef9a835014f58aa4b3e063d1d594a2e34088be37
Seungho <hsh47607@naver.com>
2023-06-26 02:09:00 +0900

    Feat: add ResponseStatus

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserController.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserController.java
@@ -3,10 +3,8 @@ package com.zerobase.hseungho.restaurantreservation.service.controller.user;
 import com.zerobase.hseungho.restaurantreservation.service.appservice.UserService;
 import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.CheckIdAvailable;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${service.basic-api}")
@@ -16,6 +14,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/sign-up/check-id")
+    @ResponseStatus(HttpStatus.OK)
     public CheckIdAvailable.Response checkIdAvailable(@RequestParam("id") String userId) {
         return CheckIdAvailable.Response.of(
                 userService.checkIdAvailable(userId)
