283bd1a71fc6a5214be165775b0881ee93132545
Seungho <hsh47607@naver.com>
2023-06-26 02:31:26 +0900

    Feat: created UserDto class

---
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/dto/internal/user/UserDto.java
@@ -0,0 +1,37 @@
+package com.zerobase.hseungho.restaurantreservation.service.dto.internal.user;
+
+import com.zerobase.hseungho.restaurantreservation.service.domain.User;
+import com.zerobase.hseungho.restaurantreservation.service.type.UserType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String id;
+    private String userId;
+    private String nickname;
+    private UserType type;
+    private LocalDateTime loggedInAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    public static UserDto fromEntity(User entity) {
+        return UserDto.builder()
+                .id(entity.getId())
+                .userId(entity.getUserId())
+                .nickname(entity.getNickname())
+                .type(entity.getType())
+                .loggedInAt(entity.getLoggedInAt())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .deletedAt(entity.getDeletedAt())
+                .build();
+    }
+
+}
