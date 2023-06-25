76c91b77a8dd7e5331d8d3b15b1c15cf88d74ce5
Seungho <hsh47607@naver.com>
2023-06-26 02:30:55 +0900

    Chore: add Sign Up Response

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/dto/external/user/SignUp.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/dto/external/user/SignUp.java
@@ -1,9 +1,7 @@
 package com.zerobase.hseungho.restaurantreservation.service.dto.external.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.zerobase.hseungho.restaurantreservation.service.dto.internal.user.UserDto;
+import lombok.*;
 
 public class SignUp {
     @Getter
@@ -16,7 +14,23 @@ public class SignUp {
         private String nickname;
     }
 
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @Builder
     public static class Response {
+        private String userId;
+        private String nickname;
+        private String userType;
+        private String createdAt;
 
+        public static Response fromDto(UserDto dto) {
+            return Response.builder()
+                    .userId(dto.getUserId())
+                    .nickname(dto.getNickname())
+                    .userType(dto.getType().name())
+                    .createdAt(dto.getCreatedAt().toString())
+                    .build();
+        }
     }
 }
