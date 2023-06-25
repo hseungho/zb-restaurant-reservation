6a1cad202f72bf46aa79df577ea43a106743b1f0
Seungho <hsh47607@naver.com>
2023-06-26 02:08:47 +0900

    Chore: add comments

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserService.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserService.java
@@ -2,6 +2,11 @@ package com.zerobase.hseungho.restaurantreservation.service.appservice;
 
 public interface UserService {
 
+    /**
+     * 아이디 중복확인 메소드.
+     * @param userId 중복확인할 아이디
+     * @return 사용가능하면 true, 사용불가면 false
+     */
     boolean checkIdAvailable(String userId);
     
 }
