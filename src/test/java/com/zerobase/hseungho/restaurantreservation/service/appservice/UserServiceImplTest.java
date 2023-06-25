c02e16d9add93102c54e15d1261af3affcc666a8
Seungho <hsh47607@naver.com>
2023-06-26 04:46:29 +0900

    Test: sign up biz logic and validation

--- src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplTest.java
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.zerobase.hseungho.restaurantreservation.service.appservice;
 
+import com.zerobase.hseungho.restaurantreservation.global.exception.impl.BadRequestException;
+import com.zerobase.hseungho.restaurantreservation.global.exception.model.ErrorCodeType;
 import com.zerobase.hseungho.restaurantreservation.service.domain.User;
 import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.SignUp;
 import com.zerobase.hseungho.restaurantreservation.service.dto.internal.user.UserDto;
@@ -34,7 +36,7 @@ class UserServiceImplTest {
     private UserServiceImpl userService;
 
     @Test
-    @DisplayName("UserService - 아이디 중복확인 사용가능")
+    @DisplayName("아이디 중복확인 사용가능")
     void test_checkIdAvailable_success() {
         // given
         given(userRepository.existsByUserId(anyString()))
@@ -46,7 +48,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("UserService - 아이디 중복확인 사용불가")
+    @DisplayName("아이디 중복확인 사용불가")
     void test_checkIdAvailable_failure() {
         // given
         given(userRepository.existsByUserId(anyString()))
@@ -58,7 +60,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("userService - 닉네임 중복확인 사용가능")
+    @DisplayName("닉네임 중복확인 사용가능")
     void test_checkNicknameAvailable_success() {
         // given
         given(userRepository.existsByNickname(anyString()))
@@ -70,7 +72,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("UserService - 닉네임 중복확인 사용불가")
+    @DisplayName("닉네임 중복확인 사용불가")
     void test_checkNicknameAvailable_failure() {
         // given
         given(userRepository.existsByNickname(anyString()))
@@ -82,7 +84,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("UserService - 회원가입 성공")
+    @DisplayName("회원가입 성공")
     void test_signUp_success() {
         // given
         final String USER_ID = "mock_user_id";
@@ -101,9 +103,9 @@ class UserServiceImplTest {
                 );
         ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
         // when
-        UserDto userDto = userService.signUp(SignUp.Request.builder()
-                .userId("").password("").nickname("")
-                .build());
+        UserDto userDto = userService.signUp(
+                signUpRequestBuilder(true, true, true)
+                        .build());
         // then
         verify(userRepository, times(1)).save(captor.capture());
         Assertions.assertNotNull(userDto);
@@ -112,5 +114,195 @@ class UserServiceImplTest {
         Assertions.assertEquals(UserType.ROLE_CUSTOMER, userDto.getType());
     }
 
+    @Test
+    @DisplayName("회원가입 실패 - 유저 아이디 Blank")
+    void test_signUp_failure_cause_userIdBlank() {
+        // given
+        // when
+        // then
+        BadRequestException exception = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(false, true, true)
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_BLANK, exception.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 Blank")
+    void test_signUp_failure_cause_passwordBlank() {
+        // given
+        // when
+        // then
+        BadRequestException exception = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, false, true)
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_BLANK, exception.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 닉네임 Blank")
+    void test_signUp_failure_cause_nicknameBlank() {
+        // given
+        // when
+        // then
+        BadRequestException exception = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(false, true, true)
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_BLANK, exception.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 아이디 중복")
+    void test_signUp_failure_cause_idDuplication() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(true);
+        // when
+        // then
+        BadRequestException exception = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, true, true)
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_USER_ID_DUPLICATED, exception.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 닉네임 중복")
+    void test_signUp_failure_cause_nicknameDuplication() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(true);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, true, true)
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_NICKNAME_DUPLICATED, ex.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 닉네임 길이")
+    void test_signUp_failure_cause_nicknameLength() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, true, false)
+                                .nickname("qwetewladfjlkfjsaffjasldfkj")
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_NICKNAME_LENGTH, ex.getErrorCodeType());
+    }
+    
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 정규식 실패 - 8자리 이하")
+    void test_signUp_failure_cause_passwordInvalidRegex_1() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, false, true)
+                                .password("test12!")
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_PASSWORD, ex.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 정규식 실패 - 특수문자 없음")
+    void test_signUp_failure_cause_passwordInvalidRegex_2() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, false, true)
+                                .password("test123412")
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_PASSWORD, ex.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 정규식 실패 - 공백 포함")
+    void test_signUp_failure_cause_passwordInvalidRegex_3() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, false, true)
+                                .password("test  1234 !")
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_PASSWORD, ex.getErrorCodeType());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 정규식 실패 - 아이디 포함")
+    void test_signUp_failure_cause_passwordInvalidRegex_4() {
+        // given
+        given(userRepository.existsByUserId(anyString()))
+                .willReturn(false);
+        given(userRepository.existsByNickname(anyString()))
+                .willReturn(false);
+        // when
+        // then
+        BadRequestException ex = Assertions.assertThrows(
+                BadRequestException.class,
+                () -> userService.signUp(
+                        signUpRequestBuilder(true, false, true)
+                                .password("test_id1234!")
+                                .build()
+                ));
+        Assertions.assertEquals(ErrorCodeType.BAD_REQUEST_SIGN_UP_PASSWORD, ex.getErrorCodeType());
+    }
+
+    private SignUp.Request.RequestBuilder signUpRequestBuilder(boolean existUserId,
+                                         boolean existPassword,
+                                         boolean existNickname) {
+        SignUp.Request.RequestBuilder builder = SignUp.Request.builder();
+        if (existUserId) builder.userId("test_id");
+        if (existPassword) builder.password("tester1234!");
+        if (existNickname) builder.nickname("test_nick");
+        return builder;
+    }
 
 }
