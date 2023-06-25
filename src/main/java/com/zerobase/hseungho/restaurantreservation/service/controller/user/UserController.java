f247141e5c9df521d17be70fe1d82230685cdd7b
Seungho <hsh47607@naver.com>
2023-06-26 02:31:11 +0900

    Feat: add Sign Up API endpoint

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserController.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserController.java
@@ -2,8 +2,10 @@ package com.zerobase.hseungho.restaurantreservation.service.controller.user;
 
 import com.zerobase.hseungho.restaurantreservation.service.appservice.UserService;
 import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.CheckUsingResourceAvailable;
+import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.SignUp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,12 +31,12 @@ public class UserController {
         );
     }
 
-//    @PostMapping("/sign-up")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public SignUp.Response signUp(@RequestBody @Validated SignUp.Request request) {
-//        return SignUp.Response.of(
-//                userService.signUp(request)
-//        );
-//    }
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public SignUp.Response signUp(@RequestBody @Validated SignUp.Request request) {
+        return SignUp.Response.fromDto(
+                userService.signUp(request)
+        );
+    }
 
 }
