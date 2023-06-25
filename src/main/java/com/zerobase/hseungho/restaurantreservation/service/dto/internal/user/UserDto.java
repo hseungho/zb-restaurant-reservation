4318eb3d0731d2692fa405cd2333d6e8be7c5008
hseungho <hsh47607@naver.com>
2023-06-26 05:01:53 +0900

    Test: sign up api test

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/dto/internal/user/UserDto.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/dto/internal/user/UserDto.java
@@ -6,7 +6,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Builder(access = AccessLevel.PRIVATE)
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
--- src/test/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserControllerTest.java
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/controller/user/UserControllerTest.java
@@ -3,7 +3,11 @@ package com.zerobase.hseungho.restaurantreservation.service.controller.user;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.hseungho.restaurantreservation.global.config.SecurityConfiguration;
 import com.zerobase.hseungho.restaurantreservation.global.security.jwt.JwtAuthenticationFilter;
+import com.zerobase.hseungho.restaurantreservation.global.util.SeoulDateTime;
 import com.zerobase.hseungho.restaurantreservation.service.appservice.UserServiceImpl;
+import com.zerobase.hseungho.restaurantreservation.service.dto.external.user.SignUp;
+import com.zerobase.hseungho.restaurantreservation.service.dto.internal.user.UserDto;
+import com.zerobase.hseungho.restaurantreservation.service.type.UserType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +20,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,7 +52,7 @@ class UserControllerTest {
     private String BASIC_API;
     
     @Test
-    @DisplayName("UserController - 아이디 중복확인 성공 (true)")
+    @DisplayName("아이디 중복확인 성공 (true)")
     void test_checkUserIdAvailable_success_true() throws Exception {
         // given
         final String url = BASIC_API + "/sign-up/check-id?id=mocktest12341234";
@@ -59,7 +67,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("UserController - 아이디 중복확인 성공 (false)")
+    @DisplayName("아이디 중복확인 성공 (false)")
     void test_checkUserIdAvailable_success_false() throws Exception {
         // given
         final String url = BASIC_API + "/sign-up/check-id?id=mocktest12341234";
@@ -74,7 +82,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("UserController - 닉네임 중복확인 성공 (true)")
+    @DisplayName("닉네임 중복확인 성공 (true)")
     void test_checkNicknameAvailable_success_true() throws Exception {
         // given
         final String url = BASIC_API + "/sign-up/check-nickname?nickname=테스트닉네임입니다";
@@ -89,7 +97,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("UserController - 닉네임 중복확인 성공 (false)")
+    @DisplayName("닉네임 중복확인 성공 (false)")
     void test_checkNicknameAvailable_success_false() throws Exception {
         // given
         final String url = BASIC_API + "/sign-up/check-nickname?nickname=테스트닉네임입니다";
@@ -101,6 +109,39 @@ class UserControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.result").value(false));
+    }
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void test_signUp_success() throws Exception {
+        // given
+        final String url = BASIC_API + "/sign-up";
+        LocalDateTime now = SeoulDateTime.now();
+        given(userService.signUp(any()))
+                .willReturn(UserDto.builder()
+                                .id("id")
+                                .userId("user_id")
+                                .nickname("nickname")
+                                .type(UserType.ROLE_CUSTOMER)
+                                .createdAt(now)
+                                .build());
+        // when
+        // then
+        mockMvc.perform(post(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        SignUp.Request.builder()
+                                .userId("user_id")
+                                .password("password1234!")
+                                .nickname("nickname")
+                                .build()
+                )))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId").value("user_id"))
+                .andExpect(jsonPath("$.nickname").value("nickname"))
+                .andExpect(jsonPath("$.userType").value(UserType.ROLE_CUSTOMER.name()))
+                .andExpect(jsonPath("$.createdAt").value(now.toString()));
     }
 
 }
