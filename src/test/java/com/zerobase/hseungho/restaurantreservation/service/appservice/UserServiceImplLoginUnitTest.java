d6f53ce057f5e0946118b667ec6e57daa76ed17c
Seungho <hsh47607@naver.com>
2023-06-26 15:55:25 +0900

    Test: created Login Test class

---
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplLoginUnitTest.java
@@ -0,0 +1,21 @@
+package com.zerobase.hseungho.restaurantreservation.service.appservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplLoginUnitTest {
+
+
+
+    @Test
+    @DisplayName("로그인 성공")
+    void test_login_success() {
+        // given
+        // when
+        // then
+    }
+
+}
