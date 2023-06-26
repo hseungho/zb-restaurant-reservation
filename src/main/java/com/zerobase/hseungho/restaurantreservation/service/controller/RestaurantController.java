588e94666587ba4038ee91e3371811defad4ad55
hseungho <hsh47607@naver.com>
2023-06-27 03:59:48 +0900

    Feat: added restaurant search autocomplete api endpoint to property

--- src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/RestaurantController.java
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/RestaurantController.java
@@ -2,11 +2,14 @@ package com.zerobase.hseungho.restaurantreservation.service.controller;
 
 import com.zerobase.hseungho.restaurantreservation.service.appservice.RestaurantService;
 import com.zerobase.hseungho.restaurantreservation.service.dto.restaurant.SaveRestaurant;
+import com.zerobase.hseungho.restaurantreservation.service.dto.restaurant.SearchAutocomplete;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${service.api.prefix}")
@@ -22,6 +25,12 @@ public class RestaurantController {
         return SaveRestaurant.Response.fromDto(
                 restaurantService.saveRestaurant(request)
         );
+    }
+
+    @GetMapping("${service.api.restaurant.search-auto}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<SearchAutocomplete.Response> searchAutocomplete(@RequestParam("keyword") String keyword) {
+        return restaurantService.searchAutoComplete(keyword);
     }
 
 }
