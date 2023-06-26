050800414e025df8ff3fe280c0b769046bbd4c16
hseungho <hsh47607@naver.com>
2023-06-26 21:41:44 +0900

    Test: register partner api service logic

--- src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplPartnerUnitTest.java
+++ src/test/java/com/zerobase/hseungho/restaurantreservation/service/appservice/UserServiceImplPartnerUnitTest.java
@@ -1,8 +1,57 @@
 package com.zerobase.hseungho.restaurantreservation.service.appservice;
 
+import com.zerobase.hseungho.restaurantreservation.service.domain.User;
+import com.zerobase.hseungho.restaurantreservation.service.dto.UserDto;
+import com.zerobase.hseungho.restaurantreservation.service.repository.UserRepository;
+import com.zerobase.hseungho.restaurantreservation.service.type.UserType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplPartnerUnitTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private Authentication authentication;
+
+    @BeforeEach
+    public void setAuthentication() {
+        User user = User.builder()
+                .userId("testid")
+                .password("testpassword1234!")
+                .nickname("testnickname")
+                .type(UserType.ROLE_CUSTOMER)
+                .build();
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities())
+        );
+    }
+
+    @Test
+    @DisplayName("파트너 등록 성공")
+    @WithMockUser(value = "ROLE_CUSTOMER")
+    void test_registerPartner_success() {
+        // given
+        // when
+        UserDto userDto = userService.registerPartner();
+        // then
+        Assertions.assertNotNull(userDto);
+        Assertions.assertEquals(UserType.ROLE_PARTNER, userDto.getType());
+    }
+
 }
