02a93edd0e9dd418d913c421a8ae4e95b5c6a015
hseungho <hsh47607@naver.com>
2023-06-26 22:08:00 +0900

    Chore: create restaurant controller and service class

---
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/service/controller/RestaurantController.java
@@ -0,0 +1,11 @@
+package com.zerobase.hseungho.restaurantreservation.service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${service.api.prefix}")
+@RequiredArgsConstructor
+public class RestaurantController {
+}
