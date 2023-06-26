76783da0902cc19a413e14c5a906131a86d6bb37
hseungho <hsh47607@naver.com>
2023-06-26 17:55:50 +0900

    Feat: add get user from SecurityContextHolder

---
+++ src/main/java/com/zerobase/hseungho/restaurantreservation/global/security/SecurityHolder.java
@@ -0,0 +1,12 @@
+package com.zerobase.hseungho.restaurantreservation.global.security;
+
+import com.zerobase.hseungho.restaurantreservation.service.domain.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityHolder {
+
+    public static User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
