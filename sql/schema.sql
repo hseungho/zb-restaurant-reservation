9bcd7bf8122c0c28fb8ed99de05e6b18e8693f06
Seungho <hsh47607@naver.com>
2023-06-27 20:37:15 +0900

    Infra: defined schema sql

---
+++ sql/schema.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS menu;
+CREATE TABLE `menu` (
+    id bigint not null auto_increment primary key ,
+    name varchar(50) not null ,
+    price bigint default 0 not null ,
+    restaurant_id bigint not null ,
+    FOREIGN KEY (restaurant_id) REFERENCES `restaurant` (id)
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE `review` (
+    id bigint not null auto_increment primary key ,
+    rating double default 1.0 not null ,
+    content varchar(255) not null ,
+    image_src varchar(255),
+    author_id varchar(40) not null ,
+    restaurant_id bigint not null ,
+    FOREIGN KEY (author_id) REFERENCES `users` (id),
+    FOREIGN KEY (restaurant_id) REFERENCES `restaurant` (id)
+);
+
+DROP TABLE IF EXISTS restaurant;
+CREATE TABLE `restaurant` (
+                              id bigint not null auto_increment primary key ,
+                              name varchar(100) not null ,
+                              address varchar(255) not null ,
+                              x double default 0.0 ,
+                              y double default 0.0 ,
+                              description varchar(255),
+                              open_hour int,
+                              open_minute int,
+                              close_hour int,
+                              close_minute int,
+                              count_of_tables int default 1 not null ,
+                              max_per_reservation int ,
+                              contact_number varchar(15) not null ,
+                              rating double default 0.0 ,
+                              manager_id varchar(40) not null ,
+                              created_at timestamp not null ,
+                              updated_at timestamp ,
+                              delete_req_at timestamp ,
+                              deleted_at timestamp ,
+                              FOREIGN KEY (manager_id) REFERENCES `users` (id)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE `users` (
+                         id varchar(40) not null primary key,
+                         user_id varchar(50) not null unique ,
+                         password varchar(255) not null ,
+                         nickname varchar(50) not null unique ,
+                         type varchar(20) not null ,
+                         logged_in_at timestamp,
+                         created_at timestamp not null ,
+                         updated_at timestamp,
+                         deleted_at timestamp
+);
